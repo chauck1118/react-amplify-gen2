908a5a7aa6a40877f97aebe4d1c985aaa38bbd70
Colin Hauck <colinhauck@gmail.com>
2024-04-12 12:42:28 -0400

    Rene fixed things

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
-// import amplifyconfig from "../amplifyconfiguration.json";
+import amplifyconfig from "../amplifyconfiguration.json";
 
-// Amplify.configure(amplifyconfig);
+Amplify.configure(amplifyconfig);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
